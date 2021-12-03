bba1596306e3682d9fb02b322dfce4f024470255
hojo84 <hojo84@gmail.com>
2021-12-03 19:22:47 +0100

    Create Dustbin class with its attributes, constructor & getter methods; implement methods throwOutGarbage (initial version) & emptyContents

--- src/main/java/com/codecool/wasterecycling/Dustbin.java
+++ src/main/java/com/codecool/wasterecycling/Dustbin.java
@@ -1,4 +1,39 @@
 package com.codecool.wasterecycling;
 
+import java.util.Arrays;
+
 public class Dustbin {
+
+        private final String color;
+        private Garbage[] garbageArray;
+        private int size = 0;
+
+
+        public Dustbin(String color) {
+                this.color = color;
+                this.garbageArray = new Garbage[10];
+        }
+
+        public String getColor() {
+                return color;
+        }
+
+        private int getCapacity() {
+                return garbageArray.length;
+        }
+
+        private int getSize() {
+                return size;
+        }
+
+        public void throwOutGarbage(Garbage garbage) {
+                short CAPACITY_MODIFIER = 2;
+                if (size + 1 > getCapacity())
+                        this.garbageArray = Arrays.copyOf(garbageArray, garbageArray.length * CAPACITY_MODIFIER);
+                garbageArray[size++] = garbage;
+        }
+
+        public void emptyContents() {
+                this.garbageArray = new Garbage[10];
+        }
 }
